47edc724dcdce7c39242e54053501cab311a06bd
Michael Small <chickenhat22@gmail.com>
2024-05-27 10:57:47 -0500

    feat: simple showcase of signal primatives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignalsAboutComponent } from './01 - About/signals-about.component';
+import { SignalApisComponent } from './01 - About/signal-apis.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <h1>Signals Tutorial</h1> `,
+  imports: [RouterOutlet, SignalsAboutComponent, SignalApisComponent],
+  template: `
+    <h1>Signals Tutorial</h1>
+     <app-signals-about />
+    <app-signal-apis />
+  `,
 })
 export class AppComponent {}
