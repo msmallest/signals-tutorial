e39ebfe4fa4c0075cb60ba6385dbab8157169911
Michael Small <chickenhat22@gmail.com>
2024-06-01 18:05:03 -0500

    feat: import `ngx-highlight-js`, use for double signal

--- src/app/misc-examples/misc-examples.component.ts
+++ src/app/misc-examples/misc-examples.component.ts
@@ -1,32 +1,39 @@
 import {Component, computed, signal} from '@angular/core';
 import {JsonPipe} from "@angular/common";
 import {MatButtonModule} from "@angular/material/button";
+import {HighlightJsDirective} from "ngx-highlight-js";
 
 @Component({
   selector: 'app-misc-examples',
   standalone: true,
-  imports: [JsonPipe, MatButtonModule],
+  imports: [JsonPipe, MatButtonModule, HighlightJsDirective],
   template: `
     <div id="double-example">
+      <div class="text">Updating a signal with
+        <pre highlight-js lang="typescript">.set()</pre>
+        and
+        <pre highlight-js lang="typescript">.update()</pre>.
+      </div>
+      <div class="text">The doubled value is <pre highlight-js lang="typescript">computed()</pre> any time <pre highlight-js lang="typescript">$value</pre> changes.
+      </div>
       <button mat-flat-button color="primary" (click)="value.set(value() + 1)">$value.set($value() + 1)</button>
       <button mat-flat-button color="primary" (click)="updateValue()">$value.update(v => v + 1)</button>
-      <div>
-        <pre>$value = signal(0); // {{ value() }}</pre>
-        <pre>$doubleValue = computed(() => this.$value() * 2); // {{ $doubleValue() }}</pre>
+      <div class="code-bg">
+        <pre highlight-js lang="typescript">$value = signal(0); // {{ value() }}</pre>
+        <pre highlight-js
+             lang="typescript">$doubleValue = computed(() => this.$value() * 2); // {{ $doubleValue() }}</pre>
       </div>
     </div>
   `,
   styles: `
-    button {
-      --mdc-filled-button-container-shape: 4px
-    }
-
     #double-example {
       display: flex;
       flex-direction: column;
       gap: 10px;
       align-items: center;
     }
+
+
   `
 })
 export class MiscExamplesComponent {
